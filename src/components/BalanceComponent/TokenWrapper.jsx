963e1601aa46b626f393dde0a3c8c63f844cf527
Bopchy <rorbochere@gmail.com>
2021-07-20 10:37:08 +0300

    Update <TokenWrapper /> to dynamically set action.

--- src/components/BalanceComponent/TokenWrapper.jsx
+++ src/components/BalanceComponent/TokenWrapper.jsx
@@ -6,19 +6,17 @@ import { GRAYSCALE_TITLE_ACTIVE } from "../../utils/colors";
 const Wrapper = styled.div`
   display: flex;
   width: 11.875rem;
-  height: 6.875rem;
   flex-direction: column;
   padding: 0.625rem 0.625rem 0.9375rem 0.625rem;
   background-color: ${GRAYSCALE_TITLE_ACTIVE};
-  box-sizing: border-box;
   border: 1px solid transparent;
-  border-top-left-radius: 0.625rem;
-  border-bottom-left-radius: 0.625rem;
+  border-radius: 0.625rem 0 0 0.625rem;
 `;
 
 const Title = styled(RegularText)`
   text-align: left;
   margin: 0 0 0.375rem 0;
+  text-transform: capitalize;
 `;
 
 const ImageWrapper = styled.span`
@@ -33,9 +31,11 @@ const ImageWrapper = styled.span`
   }
 `;
 
-const TokenWrapper = ({ tokenImage, name }) => (
+const TokenWrapper = ({ action, tokenImage, name }) => (
   <Wrapper>
-    <Title>Stake {name}</Title>
+    <Title>
+      {action} {name}
+    </Title>
     <ImageWrapper>
       <img src={tokenImage} alt="token" />
       <MediumText>{name}</MediumText>
