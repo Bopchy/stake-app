210655b826765c7f019adad187e32ee1d452eebd
Bopchy <rorbochere@gmail.com>
2021-07-21 16:06:12 +0300

    Make <Tabs /> flexible. Update Header margin.

--- src/components/basic/Tabs/index.jsx
+++ src/components/basic/Tabs/index.jsx
@@ -1,63 +1,21 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import StakeComponent from "../../StakeComponent";
-// styles
-// import { H5Header } from "../Text"; -- add H5 styles via css``
-import { POPPINS_BOLD } from "../../../utils/fonts";
-import {
-  OFF_WHITE,
-  PRIMARY_DEFAULT,
-  GRAYSCALE_BODY,
-  GRAYSCALE_TITLE_ACTIVE,
-} from "../../../utils/colors";
+import { Tabs, TabPanel } from "react-tabs";
+import { TabListComponent, TabComponent } from "./styles";
 
-// Refactor to make this component more generic
-
-const TabListComponent = styled(TabList)`
-  display: flex;
-  flex-direction: row;
-  list-style: none;
-  justify-content: center;
-  gap: 1.875rem;
-  margin: 0 0 1.25rem 0;
-  box-sizing: border-box;
-  border-bottom: 1px solid ${GRAYSCALE_TITLE_ACTIVE};
-`;
-
-const TabComponent = styled(Tab)`
-  ${POPPINS_BOLD}
-  cursor: pointer;
-  color: ${GRAYSCALE_BODY};
-  font-size: 1.25rem;
-  line-height: 2.1875rem;
-  padding: 0 0 0.625rem 0;
-  width: 6.25rem;
-  text-align: center;
-
-  &.tab-menu-body-selected {
-    color: ${OFF_WHITE};
-    border-bottom: 0.1875rem solid ${PRIMARY_DEFAULT};
-  }
-`;
-
-const TabMenu = ({ tabGroup }) => (
-  <Tabs className="tab-menu-body" selectedTabClassName="tab-menu-body-selected">
-    <TabListComponent>
-      {/* tabGroup.map((tab) => (
-        <Tab>{tab}</Tab>
-      )) */}
-      <TabComponent>Stake</TabComponent>
-      <TabComponent>Unstake</TabComponent>
-    </TabListComponent>
-
-    <TabPanel>
-      <StakeComponent />
-    </TabPanel>
-    <TabPanel>
-      <StakeComponent />
-    </TabPanel>
-  </Tabs>
-);
+const TabMenu = ({ tabGroup, panelGroup }) => {
+  // Panels are rendered according to their index in panelGroup
+  return (
+    <Tabs>
+      <TabListComponent>
+        {tabGroup.map((tab) => (
+          <TabComponent key={tab}>{tab}</TabComponent>
+        ))}
+      </TabListComponent>
+      {panelGroup.map((panel) => (
+        <TabPanel>{panel}</TabPanel>
+      ))}
+    </Tabs>
+  );
+};
 
 export default TabMenu;
--- src/components/basic/Text/index.jsx
+++ src/components/basic/Text/index.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import {
   POPPINS_BOLD,
   POPPINS_REGULAR,
@@ -17,6 +17,15 @@ export const H3Header = styled.h3`
   margin: 0 0 0.3125rem 0;
 `;
 
+export const H5Header = styled.h5`
+  ${POPPINS_BOLD}
+  color: ${OFF_WHITE};
+  text-align: center;
+  font-size: 1.25rem;
+  line-height: 2.1875rem;
+  padding: 0 0 0.625rem 0;
+`;
+
 export const H6Header = styled.h6`
   ${POPPINS_BOLD}
   color: ${OFF_WHITE};
@@ -24,7 +33,7 @@ export const H6Header = styled.h6`
   line-height: 1.5rem;
   text-transform: uppercase;
   text-align: center;
-  margin: 0 0 0.3125rem 0;
+  margin: 0 0 2.4375rem 0;
 `;
 
 export const RegularText = styled.p`
